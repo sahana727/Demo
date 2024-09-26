b1191da06067f07ba888293bc8b78128f825422c
sahana727 <santhoshshetty254@gmail.com>
2024-09-26 14:08:39 +0530

    demo and datatype

---
+++ Demo.java
@@ -0,0 +1,25 @@
+public class Demo{
+
+public static void main(String[] args){
+
+byte colorsInRainbow = 7;
+byte lettersOfVowels = 5;
+byte freezingPoint = 0;
+byte boilingPoint = 100;
+byte numberOfWheelsInCar = 4;
+byte roomTemperature = 25;
+byte daysInMonth = 30;
+byte numberOfRoomsInHouse = 5;
+short yearOfBirth = 2002;
+short phoneBatteryLevel = 50;
+short pagesInBook = 350;
+short examTotalScore = 95;
+short numberOfEmployees = 150;
+short graduationYear = 2024;
+short numberOfStudents = 73;
+short priceOfMovieTicket = 200;
+System.out.println(colorsInRainbow);
+colorsInRainbow = 6;
+System.out.println(colorsInRainbow );
+}
+}
