9a25e17639ae98645ba2e7c36d79201ac3c972c6
sahana727 <santhoshshetty254@gmail.com>
2024-10-14 09:29:16 +0530

    Camera details

---
+++ CameraDetail.java
@@ -0,0 +1,145 @@
+public class CameraDetail{
+	public static void main(String[] args){
+		Camera camera=new Camera();
+		camera.cameraName="Nikon";
+		camera.screenSize=2.5f;
+		camera.battery=90;
+		camera.typeOfLens="Macrolens";
+		camera.price=8000;
+
+		System.out.println("camera name:" +camera.cameraName);
+		System.out.println("screenSize:" +camera.screenSize);
+		System.out.println("battery:" +camera.battery);
+		System.out.println("typeOfLens:" +camera.typeOfLens);
+		System.out.println("price:" +camera.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera1=new Camera();
+		camera1.cameraName="SonyAlpha";
+		camera1.screenSize=3.0f;
+		camera1.battery=75;
+		camera1.typeOfLens="PrimeLens";
+		camera1.price=119000;
+
+		System.out.println("camera name:" +camera1.cameraName);
+		System.out.println("screenSize:" +camera1.screenSize);
+		System.out.println("battery:" +camera1.battery);
+		System.out.println("typeOfLens:" +camera1.typeOfLens);
+		System.out.println("price:" +camera1.price);
+		System.out.println("-----------------------------------");
+
+
+		Camera camera2=new Camera();
+		camera2.cameraName="Nikon Z50";
+		camera2.screenSize=2.5f;
+		camera2.battery=100;
+		camera2.typeOfLens="Telephotolens";
+		camera2.price=63000;
+
+		System.out.println("camera name:" +camera2.cameraName);
+		System.out.println("screenSize:" +camera2.screenSize);
+		System.out.println("battery:" +camera2.battery);
+		System.out.println("typeOfLens:" +camera2.typeOfLens);
+		System.out.println("price:" +camera2.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera3=new Camera();
+		camera3.cameraName="Canon";
+		camera3.screenSize=3.0f;
+		camera3.battery=50;
+		camera3.typeOfLens="fisheyelens";
+		camera3.price=3300;
+
+		System.out.println("camera name:" +camera3.cameraName);
+		System.out.println("screenSize:" +camera3.screenSize);
+		System.out.println("battery:" +camera3.battery);
+		System.out.println("typeOfLens:" +camera3.typeOfLens);
+		System.out.println("price:" +camera3.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera4=new Camera();
+		camera4.cameraName="DSLR";
+		camera4.screenSize=2.5f;
+		camera4.battery=90;
+		camera4.typeOfLens="primelens";
+		camera4.price=41000;
+
+		System.out.println("camera name:" +camera4.cameraName);
+		System.out.println("screenSize:" +camera4.screenSize);
+		System.out.println("battery:" +camera4.battery);
+		System.out.println("typeOfLens:" +camera4.typeOfLens);
+		System.out.println("price:" +camera4.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera5=new Camera();
+		camera5.cameraName="Panasonic";
+		camera5.screenSize=3.0f;
+		camera5.battery=80;
+		camera5.typeOfLens="Macrolens";
+		camera5.price=37000;
+
+		System.out.println("camera name:" +camera5.cameraName);
+		System.out.println("screenSize:" +camera5.screenSize);
+		System.out.println("battery:" +camera5.battery);
+		System.out.println("typeOfLens:" +camera5.typeOfLens);
+		System.out.println("price:" +camera5.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera6=new Camera();
+		camera6.cameraName="Nikon";
+		camera6.screenSize=3.0f;
+		camera6.battery=95;
+		camera6.typeOfLens="Zoomlens";
+		camera6.price=37500;
+
+		System.out.println("camera name:" +camera6.cameraName);
+		System.out.println("screenSize:" +camera6.screenSize);
+		System.out.println("battery:" +camera6.battery);
+		System.out.println("typeOfLens:" +camera6.typeOfLens);
+		System.out.println("price:" +camera6.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera7=new Camera();
+		camera7.cameraName="DSLR";
+		camera7.screenSize=2.5f;
+		camera7.battery=55;
+		camera7.typeOfLens="telephotolens";
+		camera7.price=47000;
+
+		System.out.println("camera name:" +camera7.cameraName);
+		System.out.println("screenSize:" +camera7.screenSize);
+		System.out.println("battery:" +camera7.battery);
+		System.out.println("typeOfLens:" +camera7.typeOfLens);
+		System.out.println("price:" +camera7.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera8=new Camera();
+		camera8.cameraName="Nikon";
+		camera8.screenSize=2.0f;
+		camera8.battery=99;
+		camera8.typeOfLens="Macrolens";
+		camera8.price=80000;
+
+		System.out.println("camera name:" +camera8.cameraName);
+		System.out.println("screenSize:" +camera8.screenSize);
+		System.out.println("battery:" +camera8.battery);
+		System.out.println("typeOfLens:" +camera8.typeOfLens);
+		System.out.println("price:" +camera8.price);
+		System.out.println("-----------------------------------");
+
+		Camera camera9=new Camera();
+		camera9.cameraName="Canon";
+		camera9.screenSize=3.0f;
+		camera9.battery=100;
+		camera9.typeOfLens="zoomlens";
+		camera9.price=34900;
+
+		System.out.println("camera name:" +camera9.cameraName);
+		System.out.println("screenSize:" +camera9.screenSize);
+		System.out.println("battery:" +camera9.battery);
+		System.out.println("typeOfLens:" +camera9.typeOfLens);
+		System.out.println("price:" +camera9.price);
+		System.out.println("-----------------------------------");
+
+	}
+}
