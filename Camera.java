d0ebba0ff4f6ab0d750a58623dc7d1d4ec1a7afd
sahana727 <santhoshshetty254@gmail.com>
2024-10-14 09:26:25 +0530

    camera info

---
+++ Camera.java
@@ -0,0 +1,22 @@
+public class Camera{
+	String cameraName;
+    float screenSize;
+	int battery;
+	String typeOfLens;
+	int price;
+  public static void captureImage(){
+
+  }
+  public static void openPhotos(){
+
+  }
+  public static void recordMovingImages(){
+
+  }
+  public static void focus(){
+
+  }
+  public static void connectWifi(){
+  	
+  }
+}
