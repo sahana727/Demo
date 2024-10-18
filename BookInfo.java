bc4bf040627db9df234b93423b6cfe1cc93610ad
sahana727 <santhoshshetty254@gmail.com>
2024-10-18 10:35:15 +0530

    save and update

--- BookInfo.java
+++ BookInfo.java
@@ -21,13 +21,31 @@ public class BookInfo{
 		saveBook(book6);
 		saveBook(book7);
 		saveBook(book8);
+		
+
 
 		for(int i = 0; i < bookdata.length; i++){
 			if(bookdata[i]!=null){
 				bookdata[i].bookDetails();
 			}
 		}
+        
+        System.out.println("---------------------------------------");
+		Book foundbook = findByName("Sonnet");
+		Book foundbook2 = findByName("2 States");
+		if(foundbook != null){
+			foundbook.bookDetails();
+		    foundbook2.bookDetails();
+		}
+		System.out.println("---------------------------------------");
+        updatePriceBybookName("namma book",900);
+        updatePriceBybookName("jaanu",550);
 
+		for(int i = 0; i < bookdata.length; i++){
+			if(bookdata[i]!=null){
+				bookdata[i].bookDetails();
+			}
+		}	
 	}
 
 	public static boolean saveBook(Book book){
@@ -36,12 +54,38 @@ public class BookInfo{
 			if(bookdata[i]==null){
 				bookdata[i] = book;
 				System.out.println("book is saved");
-				System.out.println("...................................");
 				return true;
 			}
 		}
         System.out.println("No space in array");
 			return false;
-
-	}
+    }
+	
+	public static Book findByName(String bookName) {
+    for (int i = 0; i < bookdata.length; i++) {
+        if (bookdata[i] != null) {
+            if (bookdata[i].bookName.equals(bookName)) {
+                System.out.println("Book found");
+                return bookdata[i];
+            }
+        }
+    }
+    System.out.println("Book not found");
+    return null; // Return null if the book is not found
 }
+
+	    public static boolean updatePriceBybookName(String bookName,int price){
+		    for(int i=0;i<bookdata.length;i++){
+			    if(bookdata[i]!= null){
+				   if(bookdata[i].bookName.equals(bookName)){
+				       bookdata[i].price = price;
+		               System.out.println("price updated"); 
+	                   return true;	
+	               }
+                }
+            }
+            System.out.println("not found");
+	         return false;
+        }
+    
+}        
